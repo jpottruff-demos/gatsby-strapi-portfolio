d677a373c8a9e061f754875f0c486d37b651d656
Jeff Pottruff <jeffpottruff@gmail.com>
2020-08-17 18:17:23 -0400

    ADD projects type from strapi. ADD query to retrieve featured project and send data into projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,45 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+export default ({data}) => {
+  
+  console.log(data);
+  // Destructuring
+  // giving an alias of 'projects' to the returned nodes
+  const {allStrapiProjects: { nodes: projects }} = data;
+
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: {featured: {eq: true}}) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
